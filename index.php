496f6910b06e1fdaa73878dbdf406a99636174d8
Niklas Berend <niklas.berend@gmail.com>
2014-01-18 13:58:04 +0100

    Intial stuff

---
+++ index.php
@@ -0,0 +1,202 @@
+<html>
+<head>
+<link href='http://fonts.googleapis.com/css?family=Quicksand:300,400,700' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="css/youtube-video-gallery.css" type="text/css"/>
+<meta charset="utf-8" content="encoding"/>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/jquery_easing.js"></script>
+<script type="text/javascript" src="js/jquery.fancybox.js"></script>
+<script type="text/javascript" src="js/jquery.youtubevideogallery.js"></script>
+<script type="text/javascript">
+
+	$(document).ready(function(e) {
+		
+		$(".nav li").bind("click", function() {
+			
+			to_element = $("#" + $(this).attr("rel"));
+			
+			$("html, body").animate({
+				
+				scrollTop: $(to_element).offset().top
+				
+			},1000,"easeOutQuint");
+		});
+		
+		/*youtube-gallery Start*/
+		$(".youtube-video-gallery").youtubeVideoGallery();
+	  
+	 	/*youtube-gallery End*/
+	 
+		
+		/* Fancybox Start */
+		$(".fancyzoom").fancybox({
+			openEffect : 'elastic',
+			closeEffect	: 'elastic',
+			closeClick: true,
+			helpers : {
+				title : {
+					type : 'inside'
+				}
+			}
+		});
+		/* Fancybox Ende */
+    });
+
+</script>
+<link rel="stylesheet" href="css/jquery.fancybox.css"/>
+<style type="text/css">
+
+
+	
+
+	body {
+		margin: 0;
+		padding: 0;
+	}
+	
+
+	#paging {
+		margin: 0;
+		padding: 40px 0 0 0;
+		style-type: none;
+		width: 100%;
+		position: relative;
+		left: 0;
+		font-family: 'Quicksand', sans-serif;
+	}
+	
+	#paging > li {
+		width: 100%;
+		min-height: 600px;
+	}
+	body .container {
+		width: 100%;
+		height: 100%;
+	}
+	.nav {
+		position: fixed;
+		top: 0;
+		width: 100%;
+		height: 40px;
+		z-index: 500;
+		background-color: #ccc;
+		font-family: 'Quicksand', sans-serif;
+	}
+	.nav ul {
+		margin: 0;
+		padding: 0;
+		list-style: none;
+		margin: 0 auto;
+		display: block;
+		max-width: auto;
+	}
+	.nav ul li {
+		display: inline-block;
+		height: 40px;
+		padding: 0 20px;
+		line-height: 40px;
+		background-color: #ccc;
+		cursor: pointer;
+		font-size: 16px;
+	}
+	.nav ul li:hover {
+		background-color: #000;
+		color: #fff;
+	}
+	
+	
+	#Mitwirkende {
+		background-color: #0C9;
+	}
+	
+	#Collectorspace {
+		background-color: #748412;
+	}
+	
+	#Makerspace {
+		background-color: #dd7683;
+	}
+	
+	#Reflectorspace {
+		background-color: #2e4f68;
+	}
+	#supportspace {
+		background-color: #a2abae;
+	}
+	
+	
+	.wrapper {
+		max-width: 840px;
+		padding: 20px 60px;
+		margin: 0 auto;
+	}
+	.wrapper img {
+		width: 180px;
+		margin-right: 20px;
+	}
+	#player {
+		width: 300px;
+	}
+	
+	iframe {
+		border:3px solid black;
+		max-height: 600px;
+		max-width:	840px;
+	}
+	
+	.twitter-timeline {
+		margin-left: 750px;
+		margin-right: 50px;
+		margin-top: -450px;
+		heigth: 400px;
+		width: 300px;
+		position: relative;
+		border: 1px dottet grey;
+	}
+		
+		
+</style>
+</head>
+<body>
+
+	
+       
+	<div class="container">
+    	<nav class="nav">
+        	<ul>
+            	<?php
+				
+					$navi_xml = simplexml_load_file("pages/navi.xml");
+					
+					foreach($navi_xml->children() as $elem) {
+						
+						print '<li rel="'.$elem->name.'">'.$elem->name.'</li>';
+					}
+				
+				?>
+            </ul>
+        </nav>
+        <ul id="paging">
+			<?php
+			
+				$navi_xml = simplexml_load_file("pages/navi.xml");
+					
+				foreach($navi_xml->children() as $elem) {
+					
+					print '<li id="'.$elem->name.'">';
+					print '<div class="wrapper">';
+					
+					include ("pages/".$elem->link);
+							
+					print '</div>';
+					print '</li>';
+				}
+                
+            ?>
+            </li>
+        </ul>
+        
+
+    </div>
+</body>	
+</html>
---
+++ pages/Makerspace.php
@@ -0,0 +1,10 @@
+</br>
+<h1>Galerie der Beispiele</h1>
+<img src="img/joystick.png" align="left" vspace="10" hspace="20"/>
+
+<p style="text-align:justify">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+<p style="text-align:justify"> Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, </p>
+<video id="player" poster="http://nano.hairdreams.de/wp-content/uploads/2013/12/hairdreams.jpg" loop="" autoplay="autoplay">
+  <source src="http://nano.hairdreams.de/wp-content/uploads/2013/12/hairdreams.mp4" type="video/mp4">
+  <source src="http://nano.hairdreams.de/wp-content/uploads/2013/12/hairdreams.webm" type="video/webm">
+</video>
