96ecea9b71b2f51847c09a3e6a9649b57d0d53b2
NiklasBerend <niklas.berend@gmail.com>
2014-01-23 01:36:44 +0100

    propening

--- preopening/index.php
+++ preopening/index.php
@@ -41,13 +41,8 @@
 		margin-left: 400px;
 	}
 	
-	.g-page {
-		margin: 0 auto;
-		margin-left: 800px;
-		margin-bottom: 100px;
 		
-	}
-		
+
 		
 </style>
 </head>
@@ -68,5 +63,3 @@
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 
 </div>
-
-<div class="g-page" data-width="277" data-href="//plus.google.com/107891845940983654015" data-layout="landscape" data-rel="publisher"></div>
