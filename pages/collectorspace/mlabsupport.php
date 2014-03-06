e5aa842a5ac748d57fe2ce6ec829ca04331bf8d4
NiklasBerend <niklas.berend@gmail.com>
2014-03-06 23:33:28 +0100

    playlists added as exhibits for collectorspace

---
+++ pages/collectorspace/mlabsupport.php
@@ -0,0 +1,44 @@
+<?php
+
+	$path_up = "../../";
+	
+	include ($path_up."php/helper.php");
+?>
+<div class="wrapper">
+    <h1>Playlist #mlab14-Support</h1>
+ 
+        <div class="info">
+    		<div class="left">
+        		<div class="iframe-container">
+   					<iframe src="//www.youtube.com/embed/videoseries?list=PLCp_p-hQQ3jQ17VUP8MwEubHlfD7krE1s" style="float: left; margin: 0 0 20 0;" allowfullscreen></iframe>
+      			</div>
+                     <div class="creator">
+                    	<strong>Urheber:</strong> Niklas Berend
+                    </div>
+                    <div class="license">
+	      				<a rel="license" href="http://creativecommons.org/licenses/by/4.0/" target="_blank"><img alt="Creative Commons Lizenzvertrag"                         style="border-width:0" src="http://i.creativecommons.org/l/by/4.0/80x15.png" /></a>
+                      
+                        <span>Dieses Werk ist lizenziert unter einer <a rel="license" href="http://creativecommons.org/licenses/by/4.0/" target="_blank">    	                Creative Commons Namensnennung 4.0 International Lizenz</a>.</span>
+     				</div>
+            </div>
+            <div class="right">
+                <h3>Tags</h3>
+                <ul>
+                	<li><span>///</span></li>
+                    <li><span>#mlab14</span></li>
+                    <li><span>MLab-Support</span></li>
+                    <li><span>Playlist</span></li>
+                    <li><span>Kinderzimmer</span></li>
+                    <li><span>Productions</span></li>
+                    <li><span>\\\</span></li>
+                    
+                </ul>
+            </div>
+          </div>
+    <hr/>
+	<h2>Über das Exponat</h2>
+    <p style="text-align: justify">Diese Playlist enthält die wichtigsten Erklärvideos zu #mlab14 und soll den Teilnehmern den Einstieg in den Offenen Online Kurs "Kinderzimmer Productions" des MediaLiteracyLabs erleichtern. 
+</p>
+
+       
+</div>
---
+++ pages/collectorspace/mlabtalks.php
@@ -0,0 +1,44 @@
+<?php
+
+	$path_up = "../../";
+	
+	include ($path_up."php/helper.php");
+?>
+<div class="wrapper">
+    <h1>Playlist der #mlabtalks</h1>
+ 
+        <div class="info">
+    		<div class="left">
+        		<div class="iframe-container">
+   					<iframe src="//www.youtube.com/embed/videoseries?list=PLUymajA6PlKAYPSUsCJKY2DzZs-pWUs41" style="float: left; margin: 0 0 20 0;" allowfullscreen></iframe>
+      			</div>
+                     <div class="creator">
+                    	<strong>Urheber:</strong> Medialiteracylab 
+                    </div>
+                    <div class="license">
+	      				<a rel="license" href="http://creativecommons.org/licenses/by/4.0/" target="_blank"><img alt="Creative Commons Lizenzvertrag"                         style="border-width:0" src="http://i.creativecommons.org/l/by/4.0/80x15.png" /></a>
+                      
+                        <span>Dieses Werk ist lizenziert unter einer <a rel="license" href="http://creativecommons.org/licenses/by/4.0/" target="_blank">    	                Creative Commons Namensnennung 4.0 International Lizenz</a>.</span>
+     				</div>
+            </div>
+            <div class="right">
+                <h3>Tags</h3>
+                <ul>
+                	<li><span>///</span></li>
+                    <li><span>#mlab14</span></li>
+                    <li><span>#mlabtalks</span></li>
+                    <li><span>Playlist</span></li>
+                    <li><span>Kinderzimmer</span></li>
+                    <li><span>Productions</span></li>
+                    <li><span>\\\</span></li>
+                    
+                </ul>
+            </div>
+          </div>
+    <hr/>
+	<h2>Über das Exponat</h2>
+    <p style="text-align: justify">Hier findet ihr alle #mlabtalks aus dem Offenen Onlinekurs Kinderzimmer Productions #mlab14. 
+</p>
+
+       
+</div>
