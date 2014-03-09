55201165006671d252ed17212a9dfea156457857
NiklasBerend <niklas.berend@gmail.com>
2014-03-09 22:43:51 +0100

    new exhibits added

---
+++ pages/collectorspace/appsfuerskinderzimmer.php
@@ -0,0 +1,52 @@
+<?php
+
+	$path_up = "../../";
+	
+	include ($path_up."php/helper.php");
+?>
+<div class="wrapper">
+    <h1>Apps für Kinderzimmer Productions</h1>
+ 
+        <div class="info">
+    		<div class="left">
+        		<div class="iframe-container">
+   					<iframe src="http://prezi.com/embed/r6pxuecy48-r/?bgcolor=ffffff&amp;lock_to_path=1&amp;autoplay=0&amp;autohide_ctrls=0&amp;features=undefined&amp;disabled_features=undefined" width="640" height="480" frameBorder="0" webkitAllowFullScreen mozAllowFullscreen allowfullscreen></iframe>
+      			</div>
+                     <div class="creator">
+                    	<strong>Urheber:</strong> Apps für Kinderzimmer Productions
+                    </div>
+                    <div class="license">
+	      				<a rel="license" href="http://creativecommons.org/licenses/by/4.0/" target="_blank"><img alt="Creative Commons Lizenzvertrag"                         style="border-width:0" src="http://i.creativecommons.org/l/by/4.0/80x15.png" /></a>
+                      
+                        <span>Dieses Werk ist lizenziert unter einer <a rel="license" href="http://creativecommons.org/licenses/by/4.0/" target="_blank">    	                Creative Commons Namensnennung 4.0 International Lizenz</a>.</span>
+     				</div>
+            </div>
+            <div class="right">
+                <h3>Tags</h3>
+                <ul>
+                	<li><span>///</span></li>
+                    <li><span>Apps</span></li>
+                    <li><span>iOS</span></li>
+                    <li><span>Android</span></li>
+                    <li><span>Tablet</span></li>
+                    <li><span>Smartphone</span></li>
+                    <li><span>iPhone</span></li>
+                    <li><span>iPad</span></li>
+                    <li><span>\\\</span></li>
+                    
+                </ul>
+            </div>
+          </div>
+    <hr/>
+	<h2>Über das Exponat</h2>
+    <p style="text-align: justify">Tablets / Smartphones sind die schweizer Messer der praktischen Medienarbeit. Sie haben technisch gesehen alles an Bord für Fotos, Videos, Trickfilme, Hörspiele, Zeichnungen, Montagen .... und was noch?
+Das einzige, was fehlt ist eine passende App! Vieles geht mit der passenden Anwendung schneller, leichter, einfacher als mit einem anderen Medium. Und manches geht vielleicht sogar ausschließlich mit einem mobilen Gerät. 
+<br />
+Die Arbeitsgruppe “Apps für Kinderzimmer Productions” hat im Dschungel der Millionen von Mobilprogrammen die Perlen gesucht und und auch einige gefunden, mit denen man kreative werden kann.
+In dieser Prezi werden die Medienprodukte vorgestellt, die Mitglieder der AG erstellt haben. Teilweise konnten wir Kinder zum Ausprobieren gewinnen. Manchmal durften wir aber auch selbst Hand anlegen, wischen, touchen, schütteln und singen!
+Viel Spaß beim Rundgang durch die kreativen Möglichkeiten mobiler Geräte!
+ 
+</p>
+
+       
+</div>
---
+++ pages/reflectorspace/readingasmaking.php
@@ -0,0 +1,45 @@
+<?php
+
+	$path_up = "../../";
+	
+	include ($path_up."php/helper.php");
+?>
+<div class="wrapper">
+    <h1>Reading as Making</h1>
+
+        <div class="info">
+    		<div class="left">
+        		<a href="http://goo.gl/5EUfhq" target="_blank"><img src="<?php print $global_url_prefix?>files/img/reading.png" /></a>
+                     
+                    <div class="creator">
+                    	<strong>Urheber:</strong> AG „Makers“ von Cory Doctorow - Lektüregruppe #mlab14
+                    </div>
+                    <div class="license">
+	      				<a rel="license" href="http://creativecommons.org/licenses/by/4.0/" target="_blank"><img alt="Creative Commons Lizenzvertrag"                         style="border-width:0" src="http://i.creativecommons.org/l/by/4.0/80x15.png" /></a>
+                      
+                        <span>Dieses Werk ist lizenziert unter einer <a rel="license" href="http://creativecommons.org/licenses/by/4.0/" target="_blank">    	                Creative Commons Namensnennung 4.0 International Lizenz</a>.</span>
+     				</div>
+            </div>
+            <div class="right">
+                <h3>Tags</h3>
+                <ul>
+                	<li><span>///</span></li>
+                    <li><span>Cory Doctorow</span></li>
+                    <li><span>Buch</span></li>
+                    <li><span>e-Book</span></li>
+                    <li><span>Makers</span></li>
+                    <li><span>Roman</span></li>
+                    <li><span>Lektüre</span></li>
+                    <li><span>freie Lizenz</span></li>
+                    <li><span>OER</span></li>
+                    <li><span>Literatur</span></li>
+                    <li><span>\\\</span></li>
+                 </ul>
+            </div>
+          </div>
+    <hr/>
+	<h2>Über das Exponat</h2>
+    <p style="text-align: justify">Wenn ein Buch nicht nur digital und gemeinschaftlich gelesen werden kann, sondern auch unter freier Lizenz steht, dann lassen sich zahlreiche Sachen damit machen, die vorher so nicht denkbar oder erlaubt waren. Wir haben anhand des Buchs „Makers“ von Cory Doctorow ein Brainstorming veranstaltet, was konket man mit dem Buch und um das Buch herum machen könnte. „Lesen als Machen“ / „Reading as Making“ sozugen. </p>
+
+       
+</div>
