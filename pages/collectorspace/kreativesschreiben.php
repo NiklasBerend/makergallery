0f72e732e3a5a4eabcbfd68193a481b46698de9d
NiklasBerend <niklas.berend@gmail.com>
2014-03-10 09:55:35 +0100

    new exhibit with a slidshare presentation

---
+++ pages/collectorspace/kreativesschreiben.php
@@ -0,0 +1,46 @@
+<?php
+
+	$path_up = "../../";
+	
+	include ($path_up."php/helper.php");
+?>
+<div class="wrapper">
+    <h1>Kreatives Schreiben</h1>
+ 
+        <div class="info">
+    		<div class="left">
+        		<div class="iframe-container">
+   					<iframe src="http://www.slideshare.net/slideshow/embed_code/32116176" width="427" height="356" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px 1px 0; margin-bottom:5px; max-width: 100%;" allowfullscreen> </iframe> <div style="margin-bottom:5px"></div>
+      			</div>
+                     <div class="creator">
+                    	<strong>Urheber:</strong> AG Kreatives Schreiben (Christian f. Freisleben-Teutscher, Bettina Harling)
+                    </div>
+                    <div class="license">
+	      				<a rel="license" href="http://creativecommons.org/licenses/by/4.0/" target="_blank"><img alt="Creative Commons Lizenzvertrag"                         style="border-width:0" src="http://i.creativecommons.org/l/by/4.0/80x15.png" /></a>
+                      
+                        <span>Dieses Werk ist lizenziert unter einer <a rel="license" href="http://creativecommons.org/licenses/by/4.0/" target="_blank">    	                Creative Commons Namensnennung 4.0 International Lizenz</a>.</span>
+     				</div>
+            </div>
+            <div class="right">
+                <h3>Tags</h3>
+                <ul>
+                	<li><span>///</span></li>
+                    <li><span>Kreatives Schreiben</span></li>
+                    <li><span>Handyroman</span></li>
+                    <li><span>SMS-Märchen</span></li>
+                    <li><span>Schreibwettbewerbe</span></li>
+                    <li><span>Twitteratur</span></li>
+                    <li><span>Mitschreibprojekte</span></li>
+                    <li><span>Buchbesprechungen</span></li>
+                    <li><span>Schreibwerkstätten</span></li>
+                    <li><span>\\\</span>                   
+                </ul>
+            </div>
+          </div>
+    <hr/>
+	<h2>Über das Exponat</h2>
+    <p style="text-align: justify">Wir haben das WWW durchsucht nach Angeboten für Kinder und Jugendliche zum Thema "Kreatives Schreiben" und sind dabei auf Portale zum Veröffentlichen von eigenen Texten und Buchbesprechungen, Blogs, Schreibwerkstätten und -wettbewerbe sowie besondere Formen wie Twitteratur, Handyromane, SMS-Märchen gestoßen.
+</p>
+
+       
+</div>
