eb63591422a34f64b6f037726a92c656aa033831
NiklasBerend <niklas.berend@gmail.com>
2014-03-10 23:39:29 +0100

    new exhibits added

---
+++ pages/reflectorspace/minecraftfuereltern.php
@@ -0,0 +1,44 @@
+<?php
+
+	$path_up = "../../";
+	
+	include ($path_up."php/helper.php");
+?>
+<div class="wrapper">
+    <h1>Minecraft für Eltern erklärt Teil I + II</h1>
+
+        <div class="info">
+    		<div class="left">
+        		<div class="iframe-container">
+   					<iframe src="//www.youtube.com/embed/videoseries?list=PLUymajA6PlKB-PxYUFzSlVVxJsrFZLeyw" style="float: left; margin: 0 0 20 0;" allowfullscreen></iframe>
+      			</div>
+                     <div class="creator">
+                    	<strong>Urheber:</strong> Guido und Paul 
+                    </div>
+                    <div class="license">
+	      				<a rel="license" href="http://creativecommons.org/licenses/by/4.0/" target="_blank"><img alt="Creative Commons Lizenzvertrag"                         style="border-width:0" src="http://i.creativecommons.org/l/by/4.0/80x15.png" /></a>
+                      
+                        <span>Dieses Werk ist lizenziert unter einer <a rel="license" href="http://creativecommons.org/licenses/by/4.0/" target="_blank">    	                Creative Commons Namensnennung 4.0 International Lizenz</a>.</span>
+     				</div>
+            </div>
+            <div class="right">
+                <h3>Tags</h3>
+                <ul>
+                	<li><span>///</span></li>
+                    <li><span>Minecraft</span></li>
+                    <li><span>Eltern</span></li>
+                    <li><span>Erkunden</span></li>
+                    <li><span>Anleitung</span></li>
+                    <li><span>\\\</span></li>
+                </ul>
+            </div>
+          </div>
+    <hr/>
+	<h2>Über das Exponat</h2>
+    <p style="text-align: justify">Mich beschäftigt seit einigen Tagen mal wieder das Thema “Digitale Medien in Kinderhänden.” Neben den Gefahrensehern und den passiven Fernsehzuschauern scheint wenig Platz für die Kreativen zu sein. Wie können Eltern ihrer Aufsichtspflicht gerecht werden und gleichzeitig die Kinder zu den Heldinnen der digitalen Welten gemacht werden, die sie sein müssten? (So formulieren es Tanja und Johnny Häusler bei ihrem epischen und sehenswerten Netzgemüse Rant auf der rp13). Oder liegt an der mangelnden Kreativität der Eltern, sich vorstellen zu können, dass Kinder keinen Unterschied zwischen spielen und lernen machen?
+Vor kurzem bin ich über ein Interview gestolpert, dass im Auftrag von Radio Essen nach einem Vortrag im Rahmen von “Elterndialog im Zentrum” am Gymnasium Am Stoppenberg in Essen aufgenommen wurde.
+
+Weiterführender Link: <a href="http://www.dotcomblog.de/digitale-medien-in-kinderhaenden-eine-frage-der-haltung/" target="_blank">http://www.dotcomblog.de</a> </p>
+
+       
+</div>
