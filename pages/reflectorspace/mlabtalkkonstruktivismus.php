a0f8b1286943b14a888a76fc85e533ae1cfbe09e
NiklasBerend <niklas.berend@gmail.com>
2014-03-16 19:12:03 +0100

    new background image and last exhibit

---
+++ pages/reflectorspace/mlabtalkkonstruktivismus.php
@@ -0,0 +1,58 @@
+<?php
+
+	$path_up = "../../";
+	
+	include ($path_up."php/helper.php");
+?>
+<div class="wrapper">
+    <h1>#mlabtalk Konstruktivismus Revisited. Ein Gespräch mit Stefan Aufenanger</h1>
+
+        <div class="info">
+    		<div class="left">
+        		<div class="iframe-container">
+   					<iframe src="//www.youtube.com/embed/_4w2_UwTvLI" style="float: left; margin: 0 0 20 0;" allowfullscreen></iframe>
+      			</div>
+                     <div class="creator">
+                    	<strong>Urheber:</strong> <a href="http://wiki.medialiteracylab.de/index.php?title=Mlab_talks" target="_blank">AG #mlabtalk</a>
+                    </div>
+                    <div class="license">
+	      				<a rel="license" href="http://creativecommons.org/licenses/by/4.0/" target="_blank"><img alt="Creative Commons Lizenzvertrag"                         style="border-width:0" src="http://i.creativecommons.org/l/by/4.0/80x15.png" /></a>
+                      
+                        <span>Dieses Werk ist lizenziert unter einer <a rel="license" href="http://creativecommons.org/licenses/by/4.0/" target="_blank">    	                Creative Commons Namensnennung 4.0 International Lizenz</a>.</span>
+     				</div>
+            </div>
+            <div class="right">
+                <h3>Tags</h3>
+                <ul>
+                	<li><span>///</span></li>
+                    <li><span>Konstruktivismus</span></li>
+                    <li><span>Konnektivismus</span></li>
+                    <li><span>Maker-Education</span></li>
+                    <li><span>Medienpädagogik</span></li>
+                    <li><span>Wissenschaft</span></li>
+                    <li><span>\\\</span></li>
+                    
+                </ul>
+            </div>
+          </div>
+    <hr/>
+	<h2>Über das Exponat</h2>
+    <p style="text-align: justify">In wenigen Jahren werden wohl die 3D-Drucker in den Kinderzimmern auf Hochtouren die Lego-Steine auswerfen. Ist das "Digital Making" endlich da, wo der Konstruktivismus es haben wollte? Können sich Kinder und Jugendliche die Welt im DIY-Prinzip gestalten? Wir sprechen in diesem #mlabtalk zum Ist-Stand und zur Zukunft des Lernens im Macher-Prinzip mit Stefan Aufenanger, Professor für Erziehungswissenschaft und Medienpädagogik an der Johannes Gutenberg-Universität Mainz.
+<br />
+<br />
+Weiterführende Links:<br/> 
+<br />
+<a href="https://www.khanacademy.org/" target="_blank">Khan Academy</a><br/>
+<a href="http://www.mobilegeeks.de/in-den-niederlanden-offnen-die-ersten-7-steve-jobs-schulen/" target="_blank">Steve Jobs- Schulen in den Niederlanden</a><br/>
+<a href="http://www.media.mit.edu/" target="_blank">MIT Media Lab Lifelong Learning </a><br/>
+<a href="http://llk.media.mit.edu/" target="_blank">MIT Lifelong Kindergarten</a><br/>
+<a href="http://www.papert.org/" target="_blank">Seymour Papert</a><br/>
+
+<br />
+<br />
+Talk-Gast: <i>Stefan Aufenanger</i> | Moderation: <i>Friederike Siller, Jasmin Bastian, Tabea Siebertz</i> | Produktion: <i>Blanche Fabri</i>
+
+</p>
+
+       
+</div>
